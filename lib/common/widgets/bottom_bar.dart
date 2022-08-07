d5fae6bf1e1db3ae52a5d1375990d1d83a05a5a3
Fernando Cordeiro <fernando_cordeiro@live.com>
2022-08-07 12:52:34 +0100

    📦 NEW: Account page app bar

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,4 +1,7 @@
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
+import 'package:amazon_clone_tutorial/features/account/screens/account_screens.dart';
+import 'package:amazon_clone_tutorial/features/home/screens/home_screen.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
@@ -9,20 +12,35 @@ class BottomBar extends StatefulWidget {
   State<BottomBar> createState() => _BottomBarState();
 }
 
+// https://youtu.be/ylJz7N-dv1E?t=12758
 class _BottomBarState extends State<BottomBar> {
   int _page = 0;
   double bottomBarWidth = 42;
   double bottomBarBorderSize = 4;
 
+  List<Widget> pages = [
+    const HomeScreen(),
+    const AccountScreen(),
+    const Center(child: Text("Cart Page")),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[_page],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _page,
         selectedItemColor: GlobalVariables.selectedNavBarColor,
         unselectedItemColor: GlobalVariables.unselectedNavBarColor,
         backgroundColor: GlobalVariables.backgroundColor,
         iconSize: 28,
+        onTap: updatePage,
         items: [
           // Home icon
           BottomNavigationBarItem(
@@ -44,27 +62,27 @@ class _BottomBarState extends State<BottomBar> {
             ),
             label: '',
           ),
-          // Shopping card
-          // BottomNavigationBarItem(
-          //   icon: Container(
-          //     width: bottomBarWidth,
-          //     decoration: BoxDecoration(
-          //       border: Border(
-          //         top: BorderSide(
-          //           color: _page == 1
-          //               ? GlobalVariables.selectedNavBarColor
-          //               : GlobalVariables.backgroundColor,
-          //           width: bottomBarBorderWidth,
-          //         ),
-          //       ),
-          //     ),
-          //     child: const Icon(
-          //       Icons.person_outline_outlined,
-          //     ),
-          //   ),
-          //   label: '',
-          // ),
           // Account icon
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 1
+                        ? GlobalVariables.selectedNavBarColor
+                        : GlobalVariables.backgroundColor,
+                    width: bottomBarBorderSize,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.person_outline_outlined,
+              ),
+            ),
+            label: '',
+          ),
+          // Shopping card
           BottomNavigationBarItem(
             icon: Container(
               width: bottomBarWidth,
@@ -78,8 +96,13 @@ class _BottomBarState extends State<BottomBar> {
                   ),
                 ),
               ),
-              child: const Icon(
-                Icons.person_outline_outlined,
+              child: Badge(
+                elevation: 0,
+                badgeContent: const Text("2"),
+                badgeColor: Colors.white,
+                child: const Icon(
+                  Icons.shopping_cart_outlined,
+                ),
               ),
             ),
             label: '',
