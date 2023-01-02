b46e79fd02b6ba6178621d16537bcdbfdd3a0c7f
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-01-02 14:03:54 +0000

    📦 NEW: Cart screen

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:amazon_clone_tutorial/features/account/screens/account_screens.dart';
+import 'package:amazon_clone_tutorial/features/cart/screens/cart_screen.dart';
 import 'package:amazon_clone_tutorial/features/home/screens/home_screen.dart';
 import 'package:amazon_clone_tutorial/providers/user_provider.dart';
 import 'package:badges/badges.dart';
@@ -22,7 +23,7 @@ class _BottomBarState extends State<BottomBar> {
   List<Widget> pages = [
     const HomeScreen(),
     const AccountScreen(),
-    const Center(child: Text("Cart Page")),
+    const CartScreen(),
   ];
 
   void updatePage(int page) {
--- server/routes/user.js
+++ server/routes/user.js
@@ -11,7 +11,7 @@ userRouter.post("/api/add-to-cart", auth, async (req, res) => {
 		const product = await Product.findById(id);
 		let user = await User.findById(req.user);
 
-		if (!user.cart.length == 0) {
+		if (user.cart.length == 0) {
 			user.cart.push({ product, quantity: 1 });
 		} else {
       // Validate if product is already in cart and increase quantity if it is
@@ -22,6 +22,26 @@ userRouter.post("/api/add-to-cart", auth, async (req, res) => {
 				user.cart.push({ product, quantity: 1 });
 			}
     }
+    user = await user.save();
+    res.json(user);
+	} catch (e) {
+		res.status(500).json({ error: e.message + e.stack });
+	}
+});
+
+userRouter.delete("/api/remove-from-cart/:id", auth, async (req, res) => {
+	try {
+		const { id } = req.params;
+		let user = await User.findById(req.user);
+
+		const index = user.cart.findIndex((item) => item.product._id.equals(id));
+		if (index != -1) {
+			if (user.cart[index].quantity > 1) {
+				user.cart[index].quantity--;
+			} else {
+				user.cart.splice(index, 1);
+			}
+		}
     user = await user.save();
     res.json(user);
 	} catch (e) {
